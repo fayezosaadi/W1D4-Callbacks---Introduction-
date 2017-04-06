dae2b57f27561bc4cb08ab90a0be3f7407322d6b
Fayez O Saadi <fayezosaadi@gmail.com>
2017-04-06 18:59:10 +0000

    Print the index of Waldo when found using forEach

--- CallbackArguments.js
+++ CallbackArguments.js
@@ -1,15 +1,37 @@
-// The second argument/parameter is expected to be a function
-function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback: I just passed the index into our call back function
-    }
+//This is the 2st solution of call-back function using forEach function.
+
+  function findWaldo(arr, found) {
+    arr.forEach(function(e,i){
+      if (e === "Waldo") {
+        found(i);
+      }
+    });
   }
-}
 
-function actionWhenFound(idx) {
 
-  console.log("found Waldo at index " + idx + '!');
-}
+  function actionWhenFound(ind) {
 
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+    console.log("found Waldo at index " + ind + '!');
+  }
+
+  findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+
+
+// The second argument/parameter is expected to be a function
+//This is the 1st solution of call-back function using a for loop method.
+
+  //   for (var i = 0; i < arr.length; i++) {
+  //     if (arr[i] === "Waldo") {
+  //       found(i);   // execute callback: I just passed the index into our call back function
+  //     }
+  //   }
+  // }
+
+  // function actionWhenFound(idx) {
+
+  //   console.log("found Waldo at index " + idx + '!');
+  // }
+
+  // findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
